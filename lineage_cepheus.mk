17fed129d5e3e54cb0b7d9a7b7a2c4edc4450856
followmsi <followmsi@gmail.com>
2023-03-06 18:43:36 +0100

    cepheus: LineageOS

--- lineage_cepheus.mk
+++ lineage_cepheus.mk
@@ -21,13 +21,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from cepheus device
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
-# Inherit some common crDroid stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# crDroid specific flags
+# Lineage specific flags
 TARGET_HAS_UDFPS := true
 
 # Device identifier

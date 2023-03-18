07a0e6af133f31f7b59b4f1cb269e8daef3e31b0
followmsi <followmsi@gmail.com>
2023-03-18 13:11:57 +0000

    cepheus: Fix WiFi
    TWRP_CEPHEUS_Fix_WiFi_v1.0.zip
    
    https://forum.xda-developers.com/t/rom-11-0-official-cepheus-crdroid-v7-22-12-09-2022.4193489/page-32#post-84792329

--- Android.mk
+++ Android.mk
@@ -113,7 +113,8 @@ $(WCNSS_MAC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	@echo "WCNSS MAC bin link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
+	# $(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
+	@echo -n "/mnt/vendor/persist/wlan_mac.bin" > "$(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/wlan_mac.bin"
 
 ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_INI_SYMLINK) $(WCNSS_MAC_SYMLINK)
 
